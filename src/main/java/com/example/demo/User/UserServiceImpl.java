d1111e30e6613acf0a4c0361b82bcac61bc98e33
jakkaphat1 <jakkaphat.w@kkumail.com>
2024-09-19 22:09:38 +0700

    Fix Role as USER

--- src/main/java/com/example/demo/User/UserServiceImpl.java
+++ src/main/java/com/example/demo/User/UserServiceImpl.java
@@ -17,10 +17,25 @@ public class UserServiceImpl implements UserService {
     @Autowired  // ใช้ @Autowired เพื่อ inject UserRepository
     private UserRepository userRepository;
 
+//    @Override
+//    public User save(UserDto userDto) {
+//        // สร้าง object User จากข้อมูลใน UserDto
+//        User user = new User(userDto.getEmail(), passwordEncoder.encode(userDto.getPassword()), userDto.getRole(), userDto.getFullname());
+//        
+//        // บันทึกผู้ใช้ลงในฐานข้อมูลผ่าน UserRepository
+//        return userRepository.save(user);
+//    }
+    
     @Override
     public User save(UserDto userDto) {
-        // สร้าง object User จากข้อมูลใน UserDto
-        User user = new User(userDto.getEmail(), passwordEncoder.encode(userDto.getPassword()), userDto.getRole(), userDto.getFullname());
+        // กำหนดค่า role เป็น "USER" โดยอัตโนมัติ
+        String role = "USER";
+        
+        // สร้าง object User จากข้อมูลใน UserDto โดยใช้ role ที่กำหนดไว้
+        User user = new User(userDto.getEmail(), 
+                             passwordEncoder.encode(userDto.getPassword()), 
+                             role, 
+                             userDto.getFullname());
         
         // บันทึกผู้ใช้ลงในฐานข้อมูลผ่าน UserRepository
         return userRepository.save(user);
